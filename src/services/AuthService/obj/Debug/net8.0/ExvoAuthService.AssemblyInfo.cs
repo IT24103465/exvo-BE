a5f53f77076397436389c23d94fbf59594b9d35b
IT24103465 <hansajaps@gmail.com>
2026-09-13 21:32:25 +0530

    Containerize backend services and add Kafka infrastructure

--- src/services/AuthService/obj/Debug/net8.0/ExvoAuthService.AssemblyInfo.cs
+++ src/services/AuthService/obj/Debug/net8.0/ExvoAuthService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExvoAuthService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09e2f601c0c6d289300f7accba80c2d78f4bd37e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e1d1dabb282e574b93595756e886132bd5fd695")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExvoAuthService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExvoAuthService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
